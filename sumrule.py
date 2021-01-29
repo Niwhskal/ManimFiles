c66377ee6b1b242eb9e21dd2dab073437fe73b88
test13234 <lakshwin045@gmail.com>
2021-01-29 18:26:25 +0530

    0 dates(img), run_times

--- sumrule.py
+++ sumrule.py
@@ -4,7 +4,7 @@ class SumRule(Scene):
     def construct(self):
         image = ImageMobject("/home/niwhskal/Pictures/table_final.png").scale(1.3) 
         self.play(FadeIn(image)) 
-
+        self.wait(3)
         image.generate_target()
         image.target.to_edge(LEFT)
         self.play(MoveToTarget(image))
@@ -18,15 +18,21 @@ class SumRule(Scene):
         rect = Rectangle(height=0.65, width=7.12, color = RED).next_to(image).shift(7.4*LEFT + 0.3*UP)
         self.play(ShowCreation(rect)) 
         self.remove(que)
+        self.wait(1)
         arrow = CurvedArrow(rect.get_edge_center(UP), introtext.get_edge_center(RIGHT),angle= -TAU/4,color = BLUE)
         self.play(ShowCreation(arrow))
-        sumtext = TextMobject(r"P(no dates, Indian) + P(1 date, Indian) + P(2 dates, Indian)").scale(0.6).next_to(introtext) 
+
+        sumtext_or = TextMobject(r"P(0 dates, Indian) OR P(1 date, Indian) OR P(2 dates, Indian)").scale(0.6).next_to(introtext) 
+
+        sumtext = TextMobject(r"P(0 dates, Indian) + P(1 date, Indian) + P(2 dates, Indian)").scale(0.6).next_to(introtext) 
         self.wait(0.5)
         self.remove(arrow)
         self.wait(1)
         finaltext = TextMobject("$= \\sum_{x \in {0,1,2}} P(X=x, Y=Indian)$").next_to(sumtext, DOWN).scale(0.6)
         rule = TextMobject(r"Sum Rule").next_to(finaltext, RIGHT).scale(0.6)
-        self.play(Write(sumtext))
+        self.play(Write(sumtext_or,run_time=4))
+        self.wait(2)
+        self.play(Transform(sumtext_or, sumtext))
         self.wait(2)
         self.play(Write(finaltext))
         self.play(ShowCreation(SurroundingRectangle(finaltext, fill_opacity=0)))
