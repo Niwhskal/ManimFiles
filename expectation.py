b7b9b81c9f7f96d3774327c3865d68c5598c0c35
test13234 <lakshwin045@gmail.com>
2021-02-01 22:34:05 +0530

    added dot animation, function transform

--- expectation.py
+++ expectation.py
@@ -25,48 +25,57 @@ class exp(GraphScene):
         self.play(ShowCreation(self.pdist))
         self.play(Write(dtext))    
 
-        eqn = TextMobject("$f(X) = X^2$", color = BLUE)
-        etext = TextMobject(r"...and feed it to:").next_to(eqn, TOP)
-        textgroup = VGroup(eqn, etext)
-        textgroup.next_to(graph_group, 4*RIGHT).scale(0.6) 
+        eqn = TexMobject("f(X) = X^2", color = GOLD).next_to(graph_group, 4*RIGHT)
+        etext = TextMobject(r"...and feed it to:").next_to(eqn, TOP).scale(0.6)
 
         self.play(Write(etext))
         self.wait(0.5)
         self.play(Write(eqn))
-        self.play(ShowCreation(SurroundingRectangle(eqn, color = GOLD)))
 
         x_vals = np.random.normal(0.0, 1.0, 10)
         dot ={}
         for n, val in enumerate(x_vals):
-           dot[n] = SmallDot(fill_opacity =0.8).move_to(self.coords_to_point(val,0)) 
-           
+           dot[n] = SmallDot().move_to(self.coords_to_point(val,0)) 
+                      
     
         self.y_axis_label ="$f(X)$" 
         self.x_axis_label = ""
         self.x_min =-4
         self.x_max = +4
         self.y_min =0
+        self.y_max = 16
         self.x_label_direction = 3*DOWN
-        self.x_leftmost_tick = self.x_max+1
-        self.y_bottom_tick = self.y_max+1
-         
+#        self.x_leftmost_tick = self.x_max+1
+#        self.y_bottom_tick = self.y_max+1
+
+        ftext = TextMobject(r"then plot f(x)") 
+        ftext.next_to(eqn, 7*RIGHT+ 1.5*TOP).scale(0.5)
+        self.play(Write(ftext))
+
         self.setup_axes()
 
-        ftext = TextMobject(r"and then plot f(x)") 
         label = TextMobject(r'X').scale(0.7)
         label.next_to(self.x_axis, DOWN)
         self.add(label)
-
+           
         freq_group = VGroup(self.x_axis, self.y_axis, self.axes, label)
-        freq_group.scale_in_place(0.4).next_to(textgroup, 7*RIGHT) 
+        freq_group.scale_in_place(0.4).next_to(eqn, 7*RIGHT) 
         for k, v in enumerate(x_vals):
             self.play(ShowCreation(dot[k]))
+            dottext = TexMobject(str(round(v,2))).next_to(dot[k], 0.2*DOWN).scale(0.5)
+            self.add(dottext)
+            if k ==0:
+                prev_eqn= eqn
+        
+            rn_rate = np.log(1+np.exp(-k))
+            new_eqn = TexMobject("f("+str(round(v,2))+") = "+str(round((v*v),2))).next_to(graph_group, 4*RIGHT)
+        
+            self.play(ReplacementTransform(prev_eqn, new_eqn), run_time =rn_rate)
             ndot = dot[k].move_to(self.coords_to_point(v, v*v)) 
             freq_group.add(ndot)
+            prev_eqn = new_eqn
+            self.remove(dottext) 
 
 
-        ftext.next_to(freq_group, 0.2*TOP).scale(0.5)
 
-        self.play(Write(ftext))
- 
         self.wait(10)
